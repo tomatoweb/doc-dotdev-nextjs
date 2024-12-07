a25fc9f18cf4d13b5d41381c41e8b9da4870f1fd
Mathias Appelmans <math.appelmans@gmail.com>
2024-12-07 18:03:57 +0100

    fix prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-if (process.env.CURRENT_ENV == 'production') {
-const prisma = new PrismaClient();
-} else {
-if (!global.prisma) {
-global.prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
-prisma = global.prisma;
-}
-export default prisma;
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma

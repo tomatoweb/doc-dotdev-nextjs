7cd7f056f4b44c376f05101dbd674af7e0700064
Mathias Appelmans <math.appelmans@gmail.com>
2024-12-17 14:39:49 +0100

    added no-caching to prisma posts call

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,45 @@
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server"
+
+export async function GET() {
+
+  const result = await prisma.post.findMany()
+
+  return NextResponse.json({
+    message: 'ok',
+    status: 200,
+    data: result
+  })
+}
+
+
+export async function POST(req: Request) {
+
+  const body = await req.json()
+
+  const {title, content} = body
+
+  console.log(title + ' ' + content)
+
+  await prisma.post.deleteMany({where: {}})
+
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      slug: title
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+      content: content
+    }
+  })
+
+  if(!result) return NextResponse.json({
+    message: 'error',
+    status: 500
+  })
+  
+  return NextResponse.json({ 
+    message: 'ok', 
+    status: 200,
+    data: result})
+}

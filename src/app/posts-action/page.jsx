ac4826b06c778b0f893162671265fd66a14a6776
Mathias Appelmans <math.appelmans@gmail.com>
2024-12-18 18:03:39 +0100

    added post-action page

--- src/app/posts-action/page.jsx
+++ src/app/posts-action/page.jsx
@@ -1,8 +1,5 @@
 import Link from "next/link";
 import prisma from "@/lib/db";
-import Form from "@/app/components/Form"
-
-export const dynamic = "force-dynamic"; // If you want no caching from posts at all
 
 export default async function PostsPage() {
 
@@ -10,8 +7,30 @@ export default async function PostsPage() {
 
   const postsCount = await prisma.post.count();
 
+  const addPost = async (formData) => {
+    "use server";
+
+    await prisma.post.create({
+      data: {
+        title: formData.get('title'),
+        slug: (formData.get('title'))
+          .replace(/\s+/g, "-")
+          .toLowerCase(),
+        content: formData.get('content')
+      }
+    })
+  }
+
   return (
     <main className="flex flex-col items-center justify-center py-8 gap-y-24 text-center bg-gray-800">
+      <form
+        action={addPost} 
+        className="flex flex-col gap-y-2 w-[300px]"
+      >
+        <input type="text" name="title" placeholder="title" className="p-2 rounded-sm bg-gray-900" />
+        <textarea rows={5} name="content" placeholder="content" className="p-2 rounded-sm bg-gray-900" />
+        <button className="bg-blue-500 py-2 text-white rounded-sm">Create post</button>
+      </form>
 
       <h1 className="text-3xl font-semibold">All Posts ({postsCount})</h1>
       <ul className="border-t border-b border-primary py-5 leading-8 ">
@@ -23,8 +42,6 @@ export default async function PostsPage() {
             </li>
         ) )}
       </ul>
-
-      <Form/>
 
     </main>
   );

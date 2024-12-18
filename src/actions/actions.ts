d45e0693e50963d77fbb2d781772b492b07072cb
Mathias Appelmans <math.appelmans@gmail.com>
2024-12-18 17:31:26 +0100

    added post-action page

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,30 +1,16 @@
 "use server";
 
-import prisma from "@/lib/db";
+//import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { headers } from 'next/headers';
 
-export async function createPost(formData: FormData) {
+export async function createPost() {
 
   const headersList = headers();
 
   console.log(headersList)
 
-  try {
-    await prisma.post.create({
-      data: {
-        title: formData.get('title') as string,
-        slug: (formData.get('content') as string)
-          .replace(/\s+/g, "-")
-          .toLowerCase(),
-        content: formData.get('content') as string
-      }
-    });    
-  } catch (e) {
-    console.log(e.message)
-    return e.message
-    
-  }
+  
 
   revalidatePath('/posts-action')
 }

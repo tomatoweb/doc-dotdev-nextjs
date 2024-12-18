4fbb9a0d86bae30d189e97ea0e1e1595b90e9b47
Mathias Appelmans <math.appelmans@gmail.com>
2024-12-18 18:07:17 +0100

    added post-action page

--- src/app/posts-action/page.jsx
+++ src/app/posts-action/page.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import prisma from "@/lib/db";
 
+export const dynamic = "force-dynamic"; // If you want no caching from posts at all
+
 export default async function PostsPage() {
 
   const posts = await prisma.post.findMany()

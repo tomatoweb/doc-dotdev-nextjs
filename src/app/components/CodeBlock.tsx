bd86cb178ca1628b769e16abd387bfe031464d23
Mathias Appelmans <math.appelmans@gmail.com>
2025-08-06 16:23:46 +0200

    added page

--- src/app/components/CodeBlock.tsx
+++ src/app/components/CodeBlock.tsx
@@ -1,5 +1,8 @@
 'use client';
 import { Copy } from '@geist-ui/icons';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 const CodeBlock = (props: { text: string; color?: string }) => {
 	return (
 		<div className="relative my-8">
@@ -8,6 +11,24 @@ const CodeBlock = (props: { text: string; color?: string }) => {
           color='lightgray'
           />
 			{props.text}
+			<SyntaxHighlighter
+        className="codeblock"
+        language="jsx"
+        style={atomDark}
+        wrapLongLines
+        customStyle={{
+            backgroundColor: "#0a0a0a",
+            opacity: "1",
+            marginTop: "-2rem",
+            paddingTop: "1.7rem"
+        }}
+        codeTagProps={{
+            style: {
+                color: props.color || 'white',
+            },
+        }}>
+        {props.text}
+      </SyntaxHighlighter>
 		</div>
 	)
 }

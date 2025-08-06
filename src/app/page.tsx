edaae7a6cf957c7a5cb34bc9eac7c2a37d343a30
Mathias Appelmans <math.appelmans@gmail.com>
2025-08-06 15:51:01 +0200

    added page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Box, Link, Typography } from "@mui/material";
-//import CodeBlock from "@/app/components/CodeBlock";
+import CodeBlock from "@/app/components/CodeBlock";
 import { lightBlue, lime } from "@mui/material/colors";
 
 const text1 =
@@ -703,7 +703,20 @@ const page = () => {
 			src/app/test/page.tsx
 			
 			<Typography>Install Icons</Typography>
-			
+			<CodeBlock text={`import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+
+export default function RootLayout(props) {
+   return (
+     <html lang="en">
+       <body>
+        <AppRouterCacheProvider>
+           {props.children}
+        </AppRouterCacheProvider>
+       </body>
+     </html>
+   );
+ }
+        `} />
 			<div>
 				{text1}
 				{text11}

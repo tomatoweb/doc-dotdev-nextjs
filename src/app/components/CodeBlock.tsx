1a136f9ccb0f9460f40758302725f6047ab7e40f
Mathias Appelmans <math.appelmans@gmail.com>
2025-08-06 16:44:40 +0200

    added page

--- src/app/components/CodeBlock.tsx
+++ src/app/components/CodeBlock.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { Copy } from '@geist-ui/icons';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodeBlock = (props: { text: string; color?: string }) => {
+
+	const codeString = '(num) => num + 1';
 
 	return (
 		<div className="relative my-8">
@@ -11,7 +15,9 @@ const CodeBlock = (props: { text: string; color?: string }) => {
           />
 			{props.text}
 
-			
+			<SyntaxHighlighter language="javascript" style={docco}>
+      {codeString}
+    </SyntaxHighlighter>
 
 		</div>
 	)

ad6888615fd34ed5bb461b93af9a0dbc1bba6a82
Mathias Appelmans <math.appelmans@gmail.com>
2025-08-06 16:17:03 +0200

    added page

--- src/app/components/CodeBlock.tsx
+++ src/app/components/CodeBlock.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Copy } from '@geist-ui/icons';
-import { Box } from '@mui/material';
 const CodeBlock = (props: { text: string; color?: string }) => {
 	return (
 		<div className="relative my-8">

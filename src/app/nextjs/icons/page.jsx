6e267a1585165c6dac2227618e73e6790e46a17a
Mathias Appelmans <math.appelmans@gmail.com>
2025-11-06 10:59:02 +0100

    Added tailwind Components page

--- src/app/nextjs/icons/page.jsx
+++ src/app/nextjs/icons/page.jsx
@@ -2,10 +2,11 @@
 
 import CodeBlock from '@/app/components/CodeBlock';
 import { Link, Typography } from '@mui/material';
+import Image from 'next/image';
 
 const PostsWithPrisma = () => {
 
-    const text = `npm i lucide-react
+	const text = `npm i lucide-react
 
 
 import { Bone } from 'lucide-react';
@@ -18,7 +19,7 @@ const App = () => {
 
 export default App;
     `;
-    const text2 = `npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+	const text2 = `npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
 
 
 import AppleIcon from '@mui/icons-material/Apple';
@@ -31,7 +32,7 @@ const App = () => {
 
 export default App;
     `;
-    const text3 = `npm install geist-icons
+	const text3 = `npm install geist-icons
 
 
 import { ChevronDown, ChevronRight } from '@geist-ui/icons';
@@ -47,32 +48,41 @@ const App = () => {
 export default App;
     `;
 
-    return (
-        <main>
-            <Typography variant="h6" component="div" sx={{ mb: 2 }}>
-                ✨ Lucide React Icons
-            </Typography>
-            <Link href={"https://lucide.dev/icons/"} sx={{color: 'link.main'}}>https://lucide.dev/icons/</Link>
-            <CodeBlock text={text} />
-            
-            <Typography variant="h6" component="div" sx={{ mb: 2 }}>
-                ✨ Material UI Icons
-            </Typography>
-            <Link href={"https://mui.com/material-ui/material-icons/"} sx={{color: 'link.main'}}>https://mui.com/material-ui/material-icons/</Link>
-            <CodeBlock text={text2} />
-            <Typography variant="h6" component="div" sx={{ mb: 2 }}>
-                ✨ Geist Icons
-            </Typography>
-            <Link href={"https://geist-ui.dev/en-us/components/icons"} sx={{color: 'link.main'}}>https://geist-ui.dev/en-us/components/icons</Link>
-            <CodeBlock text={text3} />
+	return (
+		<main>
+			<Typography variant="h6" sx={{ fontWeight: '600' }} mb={2}>
+				<div className="flex">
+					<Image src="/react.svg" width={25} height={0} className="mr-4" alt="React logo" />
+					React icons
+				</div>
+			</Typography>
+			<Link href="https://react-icons.github.io/react-icons/" target="_blank">
+				https://react-icons.github.io/react-icons/
+			</Link>
+			<Typography variant="h6" component="div" sx={{ mb: 2, mt: 6 }}>
+				✨ Lucide React Icons
+			</Typography>
+			<Link href={"https://lucide.dev/icons/"} sx={{ color: 'link.main' }}>https://lucide.dev/icons/</Link>
+			<CodeBlock text={text} />
 
-            <Typography variant="h6" component="div" sx={{ mb: 2 }}>
-                ✨ Demo
-            </Typography>
-            <Link href={"/mui/demo"} sx={{color: 'link.main'}} target="_blank">/mui/demo</Link>
-            <footer className="mb-20"></footer>
-        </main>
-    )
+			<Typography variant="h6" component="div" sx={{ mb: 2, mt: 6 }}>
+				✨ Material UI Icons
+			</Typography>
+			<Link href={"https://mui.com/material-ui/material-icons/"} sx={{ color: 'link.main' }}>https://mui.com/material-ui/material-icons/</Link>
+			<CodeBlock text={text2} />
+			<Typography variant="h6" component="div" sx={{ mb: 2, mt: 6 }}>
+				✨ Geist Icons
+			</Typography>
+			<Link href={"https://geist-ui.dev/en-us/components/icons"} sx={{ color: 'link.main' }}>https://geist-ui.dev/en-us/components/icons</Link>
+			<CodeBlock text={text3} />
+
+			<Typography variant="h6" component="div" sx={{ mb: 2, mt: 6 }}>
+				✨ Demo
+			</Typography>
+			<Link href={"/mui/demo"} sx={{ color: 'link.main' }} target="_blank">/mui/demo</Link>
+			<footer className="mb-20"></footer>
+		</main>
+	)
 }
 
 export default PostsWithPrisma
--- src/app/tailwind/components/page.jsx
+++ src/app/tailwind/components/page.jsx
@@ -40,13 +40,11 @@ const page = () => {
 					Tailwind Components examples
 				</Typography>
 			</div>
-
 			<Typography sx={{ mb: 4 }}>
 				<Link href="https://v1.tailwindcss.com/components" target="_blank">
 					https://v1.tailwindcss.com/components
 				</Link>
 			</Typography>
-
 			<div class="w-full max-w-xs">
 				<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 					<div class="mb-4">
@@ -60,7 +58,7 @@ const page = () => {
 							Password
 						</label>
 						<input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-							<p class="text-red-500 text-xs italic">Please choose a password.</p>
+						<p class="text-red-500 text-xs italic">Please choose a password.</p>
 					</div>
 					<div class="flex items-center justify-between">
 						<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
